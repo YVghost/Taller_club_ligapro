14f4a65e445ccaf81c65e5e228e46ce751caba5b
laboratorio <laboratorio@UPOAULA10718>
2025-04-08 08:19:33 -0500

    Agregar archivos de proyecto.

---
+++ Taller_club_ligapro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taller_club_ligapro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
